6d73d3c496af652c790c5d44f1a349e6b453f77f
Leo_Fan <leo_fan@cyberlink.com>
2025-08-27 14:09:59 +0800

    Add testing codes

--- yt_dlp/__main__.py
+++ yt_dlp/__main__.py
@@ -14,4 +14,43 @@ if __package__ is None and not getattr(sys, 'frozen', False):
 import yt_dlp
 
 if __name__ == '__main__':
-    yt_dlp.main()
+    from pprint import pprint
+
+    _TEST_URL = "https://www.youtube.com/watch?v=jNQXAC9IVRw"
+    argv = [
+        "--verbose",
+        "-s",
+        "-g",
+        #"-F",
+        _TEST_URL,
+        "--list-formats",
+        # "-f",
+        # "18"
+    ]
+    
+    yt_dlp.main(argv)
+
+    """
+
+    YoutubeDL = yt_dlp.YoutubeDL
+    params = {
+        'listsubtitles': True,
+    }
+    with YoutubeDL(params) as ydl:
+        resultDict = ydl.extract_info(_TEST_URL, download=False)
+
+    # pprint(resultDict['formats'])
+    # pprint([d['format_id'] for d in resultDict['formats']])
+
+
+    formats = resultDict.get('formats')
+    format_ids = map(lambda f: f.get('format_id'), formats)
+    # pprint(format_ids)
+
+    tags = ['18']
+    ffs = filter(lambda f: f.get('format_id') in tags, formats)
+    # height_list = [480]
+    # ffs = filter(lambda f: f.get('height') in height_list, formats)
+    # pprint(ffs)
+
+    """
